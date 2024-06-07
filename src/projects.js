131f039234daae1bcd18c142ba11aca72ff4a2ab
Dax Davis <daxantz@gmail.com>
2024-06-07 18:30:58 -0400

    moved delete and get project methods to projectList class

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,12 @@
-export default class Project {
+let listOfProjects = [];
+
+
+class Project {
     constructor(name){
+        this.id = Date.now().toString();
         this.name = name;
         this.toDos = [];
+        this.completed = [];
     }
 
     addTodo(toDo){
@@ -16,4 +21,39 @@ export default class Project {
     printProject(){
         console.log("Project Name: " + this.name + "\n",this.toDos);
     }
+
+    static addProjectToList(...projects){
+
+        projects.forEach(project => {
+            if(listOfProjects.find(list => list.name === project.name)){
+                console.error("project with this name already exists");
+            }else {
+                listOfProjects.push(project);
+            }
+        })
+        
+    }
+
+    editName(newName){
+        this.name = newName
+    }
+
+    getTodos(){
+        return this.toDos;
+    }
+
+    // deleteProject(id){
+    //     const indexOfObject = listOfProjects.findIndex(project => project.id === id);
+    //     listOfProjects.splice(indexOfObject, 1);
+    // }
+
+    // getProject(name){
+    //     const project = listOfProjects.find(project => project.name === name);
+    //     return project
+    // }
+
+
 }
+
+export {Project, listOfProjects}
+
